25573ec684dd01f99d7de70e2ffaac146e1d89e9
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-03-28 15:41:49 -0700

    Adds newHeader

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Jess is the champion";
+document.body.append(newHeader);
